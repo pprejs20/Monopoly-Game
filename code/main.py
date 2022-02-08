0f3d362fcfbc12d5660f9883be76cbde42dfeb19
Henry <h4p2001@gmail.com>
2022-02-08 20:56:17 +0000

    tile class constructor

--- code/main.py
+++ code/main.py
@@ -1,1 +1,17 @@
 
+class Tile:
+    def __init__(self, pos, space, group, action, buyable, cost,
+                baseRent, oneHouseRent, twoHouseRent, threeHouseRent, fourHouseRent,
+                hotelRent):
+        self.pos = pos
+        self.space = space
+        self.group = group
+        self.action = action
+        self.buyable = buyable
+        self.cost = cost
+        self.baseRent = baseRent
+        self.oneHouseRent = oneHouseRent
+        self.twoHouseRent = twoHouseRent
+        self.threeHouseRent = threeHouseRent
+        self.fourHouseRent = fourHouseRent
+        self.hotelRent = hotelRent
