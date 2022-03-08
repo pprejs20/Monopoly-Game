2c1dc24d614ade798147d37f011c7d6df430ac74
Patryk Prejs <pp331@sussex.ac.uk>
2022-03-08 17:19:24 +0000

    Code clean up, game can be initialised

--- code/game.py
+++ code/game.py
@@ -4,41 +4,27 @@ from pygame.locals import *
 from cards import load_all_cards
 from player import Player
 from tile import Tile
+import random
 
-pygame.init()
+class Game:
+    def __init__(self, no_of_players):
+        self.no_of_players = no_of_players
+        self.tiles = Tile.load_tiles_from_xlsx()
+        self.pot_cards, self.opp_cards = Game.get_cards()
+        self.players = []
 
-screen_width = 1000
-screen_hight = 1000
+    def set_up_game(self, no_of_players=2):
+        players = []
+        for i in range(no_of_players):
+            players.append(Player())
+        random.shuffle(players)
 
-screen = pygame.display.set_mode((screen_width, screen_hight))
-pygame.display.set_caption("Monopoly")
-
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-pygame.quit()
-
-def set_up_game(no_of_players = 2):
-    tiles = Tile.load_tiles_from_xlsx()
-    cards = load_all_cards()
-    players = []
-    for i in range(no_of_players):
-        players.append(Player())
-
-
-def set_up_player_order(players):
-    player_dice_rolls = []
-    for player in players:
-        player_dice_rolls.append((player.roll_dice(), player))
-    for i in range(0, len(player_dice_rolls) - 1):
-        if player_dice_rolls[i][0] > player_dice_rolls[i + 1][0]:
-            temp = player_dice_rolls[i]
-            player_dice_rolls[i] = player_dice_rolls[i + 1]
-            player_dice_rolls[i + 1] = temp
-    # TODO: Finish
+    @classmethod
+    def get_cards(cls):
+        pot_cards, opp_cards = load_all_cards()
+        random.shuffle(pot_cards)
+        random.shuffle(opp_cards)
+        return pot_cards, opp_cards
 
 
 
