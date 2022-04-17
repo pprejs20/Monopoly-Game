b3c6ba253ae30c1847b8dc49a0a9c01fd0c146c9
Patryk Prejs <pprejs22@gmail.com>
2022-04-17 15:01:51 +0100

    Made AIPlayer class

--- code/player.py
+++ code/player.py
@@ -87,3 +87,11 @@ class Player:
         string += "Laps: {}\n".format(self.laps)
         string += "------------------------------\n"
         return string
+
+
+class AIPlayer(Player):
+    player_count = 1
+
+    def __init__(self, pos=0, money=1500, propList=[], laps=0, token=None):
+        name = "AI Player" + str(AIPlayer.player_count)
+        super().__init__(name, pos, money, propList, laps, token)
