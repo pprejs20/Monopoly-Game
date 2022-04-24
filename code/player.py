34ec546468a9fe791843fd545d44b1852b69c82b
Patryk Prejs <pp331@sussex.ac.uk>
2022-04-24 18:53:49 +0100

    Added pydoc comments to the player.py file

--- code/player.py
+++ code/player.py
@@ -2,6 +2,10 @@ import random
 
 
 class Player:
+    """
+    A class which encapsulates all functionality required for a player in the game
+    """
+
     # TODO: Double check if the start money is correct
     def __init__(self, name, pos=1, money=1500, propList=None, laps=0, token=None, number=None, jailCard=0):
         self.jailCard = jailCard
@@ -23,9 +27,19 @@ class Player:
         self.monopolies = []
 
     def add_prop(self, tile):
+        """
+        Adds a property to the players property list
+        :param tile: the property to be added
+        """
+
         self.propList.append(tile)
 
     def move_player_forward(self, amount):
+        """
+        Moves the player forward by an x amount
+        :param amount: the number of spaces the player should be moved
+        """
+
         for i in range(amount):
             if self.pos == 39:
                 self.pos = 1
@@ -36,6 +50,11 @@ class Player:
                 self.pos += 1
 
     def move_player_backward(self, amount):
+        """
+        Moves the player backwards by an x amount
+        :param amount: the number of spaces the player should be moved
+        """
+
         for i in range(amount):
             if self.pos == 0:
                 self.pos = 39
@@ -43,41 +62,91 @@ class Player:
                 self.pos -= 1
 
     def set_pos(self, pos):
+        """
+        Sets the players position
+        :param pos: the position number
+        """
+
         self.pos = pos
 
     def move_player(self, amount):
+        """
+        Moves the player by a specified a
+        :param amount:
+        :return:
+        """
+
         self.pos += amount
 
     def is_jailed(self):
+        """
+        Checks if a player is jailed
+        :return: True if the player is jailed, otherwise False
+        """
+
         return self.jailed
 
     def jail(self):
+        """
+        Procedure for jailing the player
+        """
+
         self.jailed = True
         self.set_pos(11)
 
     def unjail(self):
+        """
+        Unjails the player
+        """
+
         self.jailed = False
         self.jail_term = 0
 
     def add_jail_term(self):
+        """
+        Adds a term once a player has missed a go because of being in jail
+        """
+
         self.jail_term += 1
         if self.jail_term == 2:
             self.unjail()
             print("[{}] You served your time, you are now free!".format(self.name))
 
     def deduct_money(self, amount):
+        """
+        Takes money away from the player
+        :param amount: the amount of money that will be deducted
+        """
+
         self.money -= amount
 
     def add_money(self, amount):
+        """
+        Gives the player money
+        :param amount: the amount of money to be added
+        """
+
         self.money += amount
 
     def roll_dice(self):
+        """
+        Rolls dice for the player
+        :return: 2 integers representing the numbers rolled on 2 separate die and a boolean value indicating if
+        its a double
+        """
+
         dOne = random.randint(1, 6)
         dTwo = random.randint(1, 6)
         doubles = dOne == dTwo
         return (dOne, dTwo, doubles)
 
     def buy_property(self, tile, cost=None):
+        """
+        Purchases a property for the player
+        :param tile: the property which is to be purchased
+        :param cost: optional cost value if the price is not the original price of the property
+        """
+
         if cost is None:
             cost = tile.cost
 
@@ -91,16 +160,29 @@ class Player:
             if num == tiles_of_color[tile.group]:
                 self.monopolies.append(tile.group)
 
-
-
-
     def has_monopoly(self):
+        """
+        Checks if the player has a monopoly
+        :return: True if the player has a monopoly, Fasle otherwise
+        """
+
         return True if len(self.monopolies) > 0 else False
 
     def get_monopolies(self):
+        """
+        Gets a list of monopolies owned by the player
+        :return: List of monopolies
+        """
+
         return self.monopolies
 
     def count_color(self, color):
+        """
+        Counts how many properties of a color the player owns
+        :param color: the color of the properties
+        :return: the number of properties owned of the specified color
+        """
+        
         count = 0
         for prop in self.propList:
             if prop.group == color:
@@ -108,6 +190,12 @@ class Player:
         return count
 
     def get_props_of_color(self, color):
+        """
+        Gets the properties of the same color owned by the player
+        :param color: the desired color of the properties
+        :return: a list of the properties of the color owned by the player
+        """
+
         props = []
         for prop in self.propList:
             if prop.group == color:
@@ -128,6 +216,10 @@ class Player:
 
 
 class AIPlayer(Player):
+    """
+    An extension of the Player class which can work autonomously without a human player
+    """
+
     player_count = 1
 
     def __init__(self, pos=1, money=1500, propList=None, laps=0, token=None):
