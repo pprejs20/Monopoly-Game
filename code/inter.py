7fc77c24fe9adcdae0b52a8fcb9e767d3b60e0c2
nfb31 <nfb3103@gmail.com>
2022-04-28 18:55:42 +0100

    adjusted free parking message

--- code/inter.py
+++ code/inter.py
@@ -1569,6 +1569,12 @@ class Intermediary:
         line3_rect.centerx = 937.5
         line3_rect.y = 350
         screen.blit(line3, line3_rect)
+        # create line 4
+        line4 = font2.render("Collect {} from free parking!".format(self.game.free_parking_money), True, BLACK)
+        line4_rect = line4.get_rect()
+        line4_rect.centerx = 937.5
+        line4_rect.y = line3_rect.bottom + 20
+        screen.blit(line4, line4_rect)
         # re-blit left side to show new player money
         self.reblit_left()
         pygame.display.update()
